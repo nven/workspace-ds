821c0d7844cbdee17927d63710cd3950ec60a7a8
Venkatesh <nvenkatesh.27@gmail.com>
2021-05-19 17:45:07 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "ibmcloud_api_key" {
+  description = "IBM Cloud API key"
+  type        = string
+  default = ""
+}
+
+variable "schematics_workspace_w_id" {
+  description = "Schematics Workspace ID"
+  type        = string
+  default = ""
+}
+
+variable "schematics_template_t_id" {
+  description = "Schematics Workspace Template ID"
+  type        = string
+  default = ""
+}
